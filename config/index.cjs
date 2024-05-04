f57dc63b456b0df1700a77a15da716d46571e680
longlongagovan <vanflkong@gmail.com>
2024-05-04 12:24:12 +0100

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝老婆',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oJjni6gkAuJAwJiFkGT2z0O9SAgw',
+      id: 'oJjni6vPRDE6bURK_mhHz0PWQBOc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'FHG8vyEP51V5ozHfIHsu3Isgjsex_POUKNdFtQQ-Ojc',
       festivals: [
